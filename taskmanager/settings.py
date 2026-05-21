4bffc841f278886709d16203be1511e9aa34f4be
Usama <usama.aslam@gmail.com>
2026-05-21 01:04:21 +0100

    resolve bandit issue that cause deploy on render

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
 
     'jazzmin',
-    'bandit',
+    #'bandit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
